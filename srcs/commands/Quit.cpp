3f8ffe5ffb419b8d25fa54846c190f6647785426
qordoux <quentin.odx@gmail.com>
2025-03-25 18:39:12 +0100

    improved quit command, why working with netcat and not in hexchat for broadcast? try to find

--- srcs/commands/Quit.cpp
+++ srcs/commands/Quit.cpp
@@ -6,7 +6,7 @@
 /*   By: qordoux <qordoux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/23 19:33:24 by qordoux           #+#    #+#             */
-/*   Updated: 2025/03/25 17:00:22 by qordoux          ###   ########.fr       */
+/*   Updated: 2025/03/25 18:08:17 by qordoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,16 @@ Quit::~Quit() {}
 
 void   Quit::execute(Client& client, std::vector<std::string>& args, Server& server)
 {
-	std::string quitMessage = (args.empty() ? "Client disconnected" : args[0]);
-	std::string message = ":" + client.nickname + " QUIT :" + quitMessage;
+	std::string quitMessage;
+	if (args.size() <= 1 || args[1].empty())
+		quitMessage = "Client disconnected";
+	else
+	{
+		quitMessage = args[1];
+		for (size_t i = 2; i < args.size(); ++i)
+			quitMessage += " " + args[i];
+	}
+	std::string message = ":" + client.nickname + "!" + client.username + "@localhost QUIT :" + quitMessage + "\r\n";
 
     for (std::map<std::string, Channel>::iterator it = server._channels.begin();
          it != server._channels.end(); ++it)
