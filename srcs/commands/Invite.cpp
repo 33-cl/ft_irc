61aa96dac8dcb362d56dc2537b87828fd4c1eb97
qordoux <quentin.odx@gmail.com>
2025-05-17 20:12:23 +0200

    fixed invite issues

--- srcs/commands/Invite.cpp
+++ srcs/commands/Invite.cpp
@@ -16,6 +16,8 @@ void 	Invite::execute(Client& client, std::vector<std::string>& args, Server& se
 	target = args[1];
 	channelName = args[2];
 
+	if (channelName[0] != '#' && channelName[0] != '&')
+		throw recoverable_error(ERR_BADCHANMASK(channelName));
 	if (server._channels.find(channelName) == server._channels.end())
 			throw recoverable_error(ERR_NOSUCHCHANNEL(client.nickname, channelName));
 	if (client.nickname == target)
@@ -24,13 +26,15 @@ void 	Invite::execute(Client& client, std::vector<std::string>& args, Server& se
 	Channel& channel = server._channels[channelName];
 	if (!channel.hasClient(client.socket.fd))
 		throw recoverable_error(ERR_NOTONCHANNEL(client.nickname, channelName));
-
-	if (channelName[0] != '#' && channelName[0] != '&')
-		throw recoverable_error(ERR_BADCHANMASK(channelName));
-
+	if (channel.modes['i'] && !channel.isOperator(client))
+		throw recoverable_error(ERR_NOTCHANNELOP(client.nickname, channelName));
+	int targetFd = server.getFdByNickname(target);
+	if (targetFd == -1)
+		throw recoverable_error(ERR_NOSUCHNICK(client.nickname, target));
+	if (channel.hasClient(targetFd))
+		throw recoverable_error(ERR_USERONCHANNEL(target, channelName));
 	channel.addInvite(server.find_client(target));
 	server.find_client(target).write(":" + client.get_mask() + " INVITE " + target + " " + channelName);
 	client.write(":" + server._name + " " + RPL_INVITING(client.nickname, channelName, target));
 }
-
 
